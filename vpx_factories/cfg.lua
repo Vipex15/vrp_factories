d40c9b1cf0420d5519b6fba61cef1d686e75f751
Vipex15 <73122512+Vipex15@users.noreply.github.com>
2024-05-07 00:17:26 +0300

    Update cfg.lua

--- vpx_factories/cfg.lua
+++ vpx_factories/cfg.lua
@@ -11,19 +11,10 @@ cfg.factories = {
         upgrades_location = { x = 1212.7478027344, y = -3207.9650878906, z = 5.9661269187927 }, 
         machines_buy = { x = 1218.8840332031, y = -3207.5598144531, z = 5.8394522666931 },  
         machines_location = {
-             {machines_id = 1, price = 1000, machines_location = {x = 1244.5650634766, y = -3155.3874511719, z = 5.5282306671143 }},      }
+             {machines_id = 1, price = 1000, machines_location = {x = 1244.5650634766, y = -3155.3874511719, z = 5.5282306671143 }},
+      }
     }
 }
-
-cfg.deposits = {
-    low_end = { 
-        {deposit_id = 1, deposit_name = "Low End Deposit", deposit_price = 10000, deposit_location = {x = 1190.3952636719, y = -3327.1728515625, z = 5.5285129547119}, deposit_leave = {x= 197.8153, y = -1002.293, z= -99.65749}},
-    },
-    high_end = { 
-        {deposit_id = 2, deposit_name = "High End Deposit", deposit_price = 30000, deposit_location = {x=1205.5281982422,y = -3327.0397949219,z = 5.5287461280823}, deposit_leave = {x= 231.86305236816, y = -1004.416809082, z= -98.999992370605}},
-    }
-}
-
 
 cfg.cars = {
     {car_hash = "adder",     car_name = "Adder",     price = 1000,  up_type = {type = "Sedan"}, up_engine = {type = "V2"}, up_transes = {type = "FWD"}, days =10,hours = 1, min = 1},
